0932f03b7b6c78c5672e1b4f47e8eb8b3a3bf51c
jw-smaal <usenet@gispen.org>
2026-01-09 08:21:45 +0100

    moved test script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #west build -b qemu_x86 tests/midi1_parser -t run
 #west build -b qemu_cortex_m3 tests/midi1_parser -t run
-west build -b qemu_cortex_m0 tests/midi1_parser -t run
+west build -b qemu_cortex_m0 midi1_parser -t run
